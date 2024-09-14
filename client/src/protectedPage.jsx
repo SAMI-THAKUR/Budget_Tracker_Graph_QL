2574745d68bc12116dbfd152fc6302d8b03d8800
SAMI THAKUR <2022.sami.thakur@ves.ac.in>
2024-09-14 22:42:33 +0530

    first

--- client/src/protectedPage.jsx
+++ client/src/protectedPage.jsx
@@ -1,42 +1,34 @@
 import { useQuery } from "@apollo/client";
 import { GET_AUTH_USER } from "./graphql/query/user.query";
-
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-// redux //
 import { useDispatch } from "react-redux";
 import { setUser } from "./feature/user.js";
 
 const ProtectedPage = ({ children }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
   const {
     loading: authLoading,
     error: authError,
     data: authData,
   } = useQuery(GET_AUTH_USER, {
     onError: (error) => {
-      console.error("Sign up error:", error);
+      console.error("Authentication error:", error);
     },
   });
 
   useEffect(() => {
-    if (authData && authData.authUser) {
-      dispatch(setUser(authData.authUser));
-    }
-  }, [authData, dispatch]);
-
-  useEffect(() => {
     if (!authLoading) {
-      if (authError) {
-        console.error("Authentication error:", authError);
-        navigate("/login");
-      } else if (!authData || !authData.authUser) {
+      if (authData?.authUser) {
+        dispatch(setUser(authData.authUser));
+      } else {
         console.warn("User is not authenticated.");
         navigate("/login");
       }
     }
-  }, [authLoading, authError, authData, navigate]);
+  }, [authLoading, authData, navigate, dispatch]);
 
   if (authLoading) {
     return (
@@ -48,7 +40,8 @@ const ProtectedPage = ({ children }) => {
       </div>
     );
   }
-  return <div>{children}</div>;
+
+  return authData?.authUser ? <div>{children}</div> : null;
 };
 
 export default ProtectedPage;

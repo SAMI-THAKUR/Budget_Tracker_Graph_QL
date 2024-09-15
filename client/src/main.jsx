70b851a95af8ec66f563478031ed8d37fac66d11
SAMI THAKUR <2022.sami.thakur@ves.ac.in>
2024-09-15 22:27:10 +0530

    new

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import store from "./store.js";
 
